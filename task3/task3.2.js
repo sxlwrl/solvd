566170135156b2e682c1752659c170a31ff4553c
sxlwrl <vadymkazak2019@gmail.com>
2024-04-21 19:30:01 +0300

    [add] task2

---
+++ task3/task3.2.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const checkIsArray = function (arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error('You have to pass an array');
+    }
+}
+
+const uppercaseLetter = function (string) {
+    const firstLetter = string.at(0).toUpperCase();
+    const rest = string.slice(1);
+    return `${firstLetter}${rest}`;
+};
+
+const getFullName = function (person) {
+    try {
+        if (!person.firstName || !person.lastName) {
+            throw new Error('There are no firstName or lastName properties');
+        }
+
+        return `${uppercaseLetter(person.firstName)} ${uppercaseLetter(person.lastName)}`;
+    } catch (err) {
+        return err.message;
+    }
+};
+
+const filterUniqueWords = function (str) {
+    try {
+        if (typeof str !== 'string') {
+            throw new Error('The argument type is not a string');
+        }
+
+        return str
+            .match(/\b(\w+)\b/g)
+            .filter((item, index, arr) => arr.indexOf(item) === index)
+            .sort((a, b) => a.localeCompare(b));
+    } catch (err) {
+        return err.message;
+    }
+};
+
+const calculateAverageGrade = function (grades) {
+    return grades.reduce((total, grade) => total + grade, 0) / grades.length;
+}
+
+const getGrades = function (students) {
+    return students.map(student => student.grades);
+}
+
+const getAverageGrade = function (students) {
+    try {
+        checkIsArray(students);
+        return calculateAverageGrade(getGrades(students).flat());
+    } catch (err) {
+        return err.message;
+    }
+}
+
+const studentsList = [
+    {'name': 'Alex', 'grades': [85, 74, 92]},
+    {'name': 'Julia', 'grades': [64, 84, 90]},
+    {'name': 'Nick', 'grades': [89, 95, 100]},
+    {'name': 'John', 'grades': [92, 92, 84]},
+    {'name': 'Noah', 'grades': [99, 96, 98]},
+    {'name': 'George', 'grades': [74, 95, 82]},
+];
+
+console.log(getFullName({'firstName': 'Vadym', 'lastName': 'Kazak'}));  // Vadym Kazak
+console.log(getFullName({'firstName': 'vadym', 'lastName': 'kazak'}));  // Vadym Kazak
+console.log(getFullName({'firstName': undefined, 'lastName': 'Kazak'}));    // Error
+
+console.log();
+
+console.log(filterUniqueWords('Lorem! Lorem Lorem Lorem Lorem woax ipsum dolor sit amet, consectetur adipisicing ' +
+    'elit. A beatae, illo illum magnam minus neque quae quos repellendus temporibus! Laboriosam.'));
+console.log(filterUniqueWords(true));   // Error
+
+console.log();
+
+console.log(getAverageGrade(studentsList));     // 88.056
+console.log(getAverageGrade(null));     // Error
