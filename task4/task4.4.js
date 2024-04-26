849a1b04eda8a195e198eb872e4b8767fb08dc74
sxlwrl <vadymkazak2019@gmail.com>
2024-04-26 16:42:54 +0300

    [change] added support for creating immutable arrays as values

--- task4/task4.4.js
+++ task4/task4.4.js
@@ -1,10 +1,13 @@
 'use strict';
 
+const checkIsObject = require('./utils');
+
 const person = {
     firstName: 'John',
     lastName: 'Doe',
     age: 30,
     email: 'john.doe@example.com',
+    visitedCities: ['Rome', 'Paris', 'Madrid'],
     x: {
         y: {
             z: 5,
@@ -18,15 +21,15 @@ const createImmutableObject = function (obj) {
             return obj;
         }
 
-        const newObj = {};
+        const newObj = Array.isArray(obj) ? obj.map(createImmutableObject) : {};
 
         Object
             .keys(obj)
             .forEach(key => {
-                const value = createImmutableObject(obj[key]);
+                let value = obj[key];
 
                 Object.defineProperty(newObj, key, {
-                    value,
+                    value: typeof value === 'object' ? createImmutableObject(value) : value,
                     writable: false,
                     enumerable: true,
                     configurable: false,
@@ -41,8 +44,9 @@ const createImmutableObject = function (obj) {
 
 const immutablePerson = createImmutableObject(person);
 
-console.log(Object.getOwnPropertyDescriptors(immutablePerson));     // {property: {value, writable: false, enum: true, conf: false}}
-console.log(Object.getOwnPropertyDescriptors(immutablePerson.x.y));     // z: { value: 5, writable: false, enumerable: true, configurable: false }
+console.log(Object.getOwnPropertyDescriptors(immutablePerson));                 // {property: {value, writable: false, enum: true, conf: false}}
+console.log(Object.getOwnPropertyDescriptors(immutablePerson.x.y));             // z: { value: 5, writable: false, enumerable: true, configurable: false }
+console.log(Object.getOwnPropertyDescriptors(immutablePerson.visitedCities));
 
 try {
     immutablePerson.firstName = 'test';     // Error
